2669dfd3785eae55195eaf97399bf56103dc96e5
hannahvergio <vergio.hannahgrace@gmail.com>
2025-04-03 22:22:38 +0800

    backend commit

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import task_list, task_detail
+
+urlpatterns = [
+    path('tasks/', task_list, name='task-list'),
+    path('tasks/<int:pk>/', task_detail, name='task-detail'),
+]
