b5e10f53793a11920d7bef1c3ce3b0073ba9f31c
hannahvergio <vergio.hannahgrace@gmail.com>
2025-04-04 09:36:26 +0800

    Fix: Added missing 'path' import for root URL

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,8 @@
-from django.shortcuts import redirect
-
-def redirect_to_api(request):
-    return redirect('/api/')
-
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-    path('', redirect_to_api),
-] 
+    path('', index),
+]
