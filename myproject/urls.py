9ec40ac5044ba435ba5fc738fe6c6cd9771e0233
hannahvergio <vergio.hannahgrace@gmail.com>
2025-04-04 09:32:03 +0800

    Add root view to handle base URL

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_api(request):
+    return redirect('/api/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-]
+    path('', redirect_to_api),
+] 
