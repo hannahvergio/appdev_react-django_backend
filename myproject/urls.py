e727a9e83997d528c6624de676ab8203f58f957f
hannahvergio <vergio.hannahgrace@gmail.com>
2025-04-04 10:02:09 +0800

    Fix: Resolved circular import issue with index view

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp.views import index 
+from myapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
