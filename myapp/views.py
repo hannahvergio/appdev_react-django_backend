c3da5747f86a5a4ff95fd923dca1b431972c6cee
Lucas <22201270@aluno.cotemig.com.br>
2025-04-03 23:10:49 -0300

    helps for u

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,29 +10,31 @@ def task_list(request):
     if request.method == "GET":
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == "POST":
+    if request.method == "POST":
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "Invalid data", "details": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["PUT", "DELETE"])
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
 def task_detail(request, pk):
-    try:
-        task = Task.objects.get(pk=pk)
-    except Task.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    task = get_object_or_404(Task, pk=pk)
 
-    if request.method == "PUT":
-        serializer = TaskSerializer(task, data=request.data)
+    if request.method == "GET":
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method in ["PUT", "PATCH"]:
+        serializer = TaskSerializer(task, data=request.data, partial=(request.method == "PATCH"))
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"error": "Update Error", "details": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    if request.method == "DELETE":
         task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "Task Deleted"}, status=status.HTTP_204_NO_CONTENT)
