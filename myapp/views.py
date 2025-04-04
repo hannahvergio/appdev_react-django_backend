7c95ddd815ff364f994af5258a129878c48d2b0c
hannahvergio <vergio.hannahgrace@gmail.com>
2025-04-04 09:55:13 +0800

    Fix: Resolved circular import issue with index view

--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,6 @@ from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
-def index(request):
-    return HttpResponse("Hello from the root URL!")
-
 @api_view(["GET", "POST"])
 def task_list(request):
     if request.method == "GET":
